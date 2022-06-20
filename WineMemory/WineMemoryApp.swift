5ddca6070422bd32fec7f7da14210f3d0b867d2a
Gerd Faedtke <gerd.faedtke@icloud.com>
2022-06-20 15:42:44 +0200

    Initial Commit

---
+++ WineMemory/WineMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineMemoryApp.swift
+//  WineMemory
+//
+//  Created by Gerd Faedtke on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct WineMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
